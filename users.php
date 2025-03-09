d8e7dff69db8f1dda432ee7cc9d430651af338d0
pierreemad93 <pierreemad93@outlook.com>
2025-03-09 22:43:40 +0200

    read users from db

--- users.php
+++ users.php
@@ -1,17 +1,49 @@
 <?php
-session_start();
-if (!isset($_SESSION["isLoggedIn"])) {
+//CRUD operation => Create Read Update Delete
+require_once "config/db.php";
+require_once "includes/header.php";
+if (!isset($_SESSION["IsLoggedIn"])) {
     header("Location: index.php");
     exit();
 }
 
-require_once "includes/header.php";
 require_once "includes/navbar.php";
+
+$stmt = $connect->prepare('SELECT * FROM `users` ');
+$stmt->execute();
+$users = $stmt->fetchAll();
+// echo "<pre>";
+// print_r($users);
+// echo "</pre>";
 ?>
 
 <div class="container">
     <h1 class="text-center">Users</h1>
-</div> 
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">fullname</th>
+                <th scope="col">email</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $index => $user): ?>
+                <tr>
+                    <th scope="row"><?= $index + 1 ?></th>
+                    <td><?= $user['full_name'] ?></td>
+                    <td><?= $user['email'] ?></td>
+                    <td>
+                        <a href="" class="btn btn-primary">show</a>
+                        <a href="" class="btn btn-warning">edit</a>
+                        <a href="" class="btn btn-danger">delete</a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</div>
 
 <?php
 require_once "includes/footer.php";
